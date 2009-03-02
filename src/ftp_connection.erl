0c3c4c82be238886cc0a8f22909fc9da84b25805
Christopher Bertels <bakkdoor@flasht.de>
2009-03-02 21:34:11 +0100

    code etwas verfeinert und einige syntaxfehler behoben

--- src/ftp_connection.erl
+++ src/ftp_connection.erl
@@ -13,23 +13,23 @@ receive_loop() ->
 	{quit, PortNr} ->
 	    io:format("verbindung beendet auf port: ~w~n", [PortNr]);
 
-	{send_loop_pid, SendLoop) ->
+	{send_loop_pid, SendLoop} ->
 	    spawn(ftp_connection, send_loop, [SendLoop]);
 
 	{command, Command, Parameters} ->
 	    execute_command(Command, Parameters),
-	    loop();
+	    receive_loop();
 	
 	Unknown ->
 	    io:format("unknown command: ~w~n", [Unknown]),
-	    loop()
+	    receive_loop()
     end.
 
 
 send_loop(DriverSendLoop) ->
     % schickt nachrichten an DriverSendLoop mit daten/befehlen/antworten,
     % die an den client geschickt werden sollen
-    
+    send_loop(DriverSendLoop).
 
 
 % starts a new process for a given command.
