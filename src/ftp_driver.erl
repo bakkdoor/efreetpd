5ab5eb4ccddb084e9eda658d0a3b02dca256806e
Christopher Bertels <bakkdoor@flasht.de>
2009-03-03 04:34:11 +0800

    code etwas verfeinert und einige syntaxfehler behoben
    Signed-off-by: Denis <calltopower88@web.de>

--- src/ftp_driver.erl
+++ src/ftp_driver.erl
@@ -6,13 +6,14 @@ start(PortNr, FtpConnectionPid) ->
     % und loopen
     ReceiveLoop = spawn(ftp_driver, receive_loop, [PortNr, FtpConnectionPid]),
     SendLoop = spawn(ftp_driver, send_loop, [PortNr]),
-    FtpConnectionPid ! {send_loop_pid, SendLoop).
+    FtpConnectionPid ! {send_loop_pid, SendLoop}.
 
 receive_loop(PortNr, FtpConnectionPid) ->
     % FtpConnectionPid nachrichten senden mit empfangenen befehlen usw....
     % socket abfragen und umwandeln in eigenes format
     % anschließend an FtpConnectionPid senden und wieder warten (loopen)
-    .
+    
+    receive_loop(PortNr, FtpConnectionPid).
 
 
 send_loop(PortNr) ->
@@ -22,9 +23,18 @@ send_loop(PortNr) ->
 	{command, Command, Parameters} ->
 	    Converted = convert_command(Command, Parameters),
 	    send_to_client(PortNr, Converted),
-	    send_loop();
+	    send_loop(PortNr);
 	
 	Unknown ->
 	    io:format("unknown command received in ftp_driver: ~w", [Unkown]),
-	    send_loop()
+	    send_loop(PortNr)
     end.
+
+
+% sends an FTPMessage to a given client on port PortNr.
+send_to_client(PortNr, FTPMessage) ->
+    true.
+
+
+convert_command(Command, Parameters) ->
+    true.
