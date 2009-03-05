ea8d2f05d8451bd8aac4674e0a90979c97efc64b
Christopher Bertels <bakkdoor@flasht.de>
2009-03-05 14:21:43 +0100

    ftp_driver & ftp_connection extended.

--- src/ftp_connection.erl
+++ src/ftp_connection.erl
@@ -1,35 +1,75 @@
+%% Main module for ftp_connection process.
+%% Handles an ftp connection to a connected client.
+%% Starts a ftp_driver process, that talks to the client over a socket.
+%% Also starts subprocesses for a given ftp command (e.g. uploading files, creating dirs etc.)
+%% if requested by ftp_driver process. 
 -module(ftp_connection).
 -export([start/1]).
+-author({"Christopher Bertels", "bakkdoor@flasht.de"}).
+
 
 start(PortNr) ->
-    %ftp_driver prozess starten, der auf PortNr hoert
+    % start loop & ftp_driver processes
     LoopPid = spawn(ftp_connection, receive_loop, []),
-    FtpDriverPid = spawn(ftp_driver, start, [PortNr, LoopPid]).
+    FtpDriverPid = spawn(ftp_driver, start, [PortNr, LoopPid]),
+    
+    % register processes (with PortNr as part of the name)
+    register(utils:process_name(PortNr), LoopPid),
+    register(utils:process_name("ftp_driver_", PortNr), FtpDriverPid).
 
 
+%% main loop, waiting for messages by the ftp_driver process.
+%% if a command request comes in, we start a subprocess for the requested command.
 receive_loop() ->
-    % solange loopen, wie verbindung vorhanden
+    % loop as long as connection is alive
     receive
+	% the ftp_driver has closed the connection to the client on this port.
 	{quit, PortNr} ->
-	    io:format("verbindung beendet auf port: ~w~n", [PortNr]);
+	    io:format("connection closed on port: ~w~n", [PortNr]);
 
-	{send_loop_pid, SendLoop} ->
-	    spawn(ftp_connection, send_loop, [SendLoop]);
+	% ftp_driver has told us its send_loop process, so we start our own
+	{send_loop_pid, DriverSendLoop} ->
+	    spawn(ftp_connection, send_loop, [DriverSendLoop]);
 
+	% command request to be executed
+	% start a subprocess to deal with the command
 	{command, Command, Parameters} ->
 	    execute_command(Command, Parameters),
 	    receive_loop();
 	
+	% any other messages, we simply output and keep on going...
 	Unknown ->
 	    io:format("unknown command: ~w~n", [Unknown]),
 	    receive_loop()
     end.
 
 
+%% loop for sending requests to the client via ftp_driver process.
+%% sends messages to client via DriverSendLoop (ftp_driver:send_loop) with data/commands/replies.
 send_loop(DriverSendLoop) ->
-    % schickt nachrichten an DriverSendLoop mit daten/befehlen/antworten,
-    % die an den client geschickt werden sollen
-    send_loop(DriverSendLoop).
+    % wait for messages to forward to ftp_driver
+    receive
+	{reply, Reply} ->
+	    case Reply of
+		{data_stream, _} ->
+		    io:format("sending data to client..."), % just for debugging...
+		    DriverSendLoop ! Reply,
+		    send_loop(DriverSendLoop);
+		
+		{message, Message} ->
+		    io:format("sending message to client: ~p~n", [Message]),
+		    DriverSendLoop ! Reply,
+		    send_loop(DriverSendLoop);
+		
+	        UnknownReply ->
+		    io:format("error: unknown reply format: ~p~n", [UnknownReply]),
+		    send_loop(DriverSendLoop)
+	    end;
+	
+	Unknown ->
+	    io:format("error: unkown message in send_loop (# ~p): ~p~n", [self(), Unknown]),
+	    send_loop(DriverSendLoop)
+    end.
 
 
 % starts a new process for a given command.
