b1da9956630cb42b4b8c86b638a9af8953c79225
Christopher Bertels <bakkdoor@flasht.de>
2009-03-10 20:06:34 +0100

    added setting/1 which stores settings in processe's dictionary

--- src/config.erl
+++ src/config.erl
@@ -1,14 +1,17 @@
 -module(config).
--export([root_dir/0, read_config/1, setting/2]).
+-export([root_dir/0, read/1, setting/1, setting/2]).
 -author({"Christopher Bertels", "bakkdoor@flasht.de"}).
 
 %% specifies the root dir, in which all user directories are
 root_dir() ->
     "/home/bakkdoor/projekte/erlang/eFreeTPd/root_dir".
 
+std_config_file() ->
+    "eFreeTPd.conf".
+
 % TODO
 % Functions for reading setting(s) from a config file
-read_config(_Filename) ->
+read(_Filename) ->
     %% read settings and return them as list of tuples (key-value pairs)
     %% for now, simply return the values statically...
     [
@@ -21,8 +24,32 @@ read_config(_Filename) ->
      {start_port, 10000}
     ].
 
+%% returns a specified setting-value (RequestedSetting)
+%% from a list of settings (ConfigSettings)
 setting(ConfigSettings, RequestedSetting) ->
     Filtered = lists:filter(fun ({Key, _Val}) -> Key =:= RequestedSetting end,
 			    ConfigSettings),
-    % return the first match
-    lists:nth(1, Filtered). 
+    % return the first match, if there are any
+    % otherwhise simply return undefined.
+    case Filtered of
+	[] ->
+	    undefined;
+	[_H|_T] ->
+	    {_Key, Val} = lists:nth(1, Filtered),
+	    Val % first match value.
+    end.
+
+
+%% works like setting/2, but reads the settings from a standard config-file
+%% defined in std_config_file().
+%% when first called, it saves the settings in the current processe's dictionary (via put)
+%% in any subsequent calls to this function from the same process, it simply gets the settings
+%% from the processe's dictionary (via get). the name of the key within the dictionary is: config_settings
+setting(RequestedSetting) ->
+    case get(config_settings) of
+	undefined ->
+	    put(config_settings, read(std_config_file()));
+	_Else ->
+	    nothing
+    end,
+    setting(get(config_settings), RequestedSetting).
