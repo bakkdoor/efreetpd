bb20f69ebe3e8cdd2c41f01254ac50882ad249ac
Christopher Bertels <bakkdoor@flasht.de>
2009-03-10 02:41:32 +0100

    added documentation to user module

--- src/user.erl
+++ src/user.erl
@@ -3,10 +3,13 @@
 -author({"Christopher Bertels", "bakkdoor@flasht.de"}).
 
 
+%% returns the home directory of a given user.
 home_dir(UserName) ->
     config:root_dir() ++ "/" ++ UserName.
 
 
+%% checks, if a given folder/directory belongs to a given user.
+%% returns true or false, indicating the ownership.
 check_dir(UserName, Dir) ->
     UserHomeDir = home_dir(UserName),
     case string:str(Dir, UserHomeDir) of
@@ -17,6 +20,8 @@ check_dir(UserName, Dir) ->
     end.
 
 
+%% checks, if a given file belongs to a given user.
+%% returns true or false, indicating the ownership.
 check_file(UserName, File) ->
     Dirname = filename:dirname(File),
-    user:check_dir(Dirname).
+    check_dir(Dirname).
