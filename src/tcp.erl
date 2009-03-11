b4efd21b6f304ccaf715663955d3bd3f8a9adf7e
Christopher Bertels <bakkdoor@flasht.de>
2009-03-11 02:23:35 +0100

    added tcp module for some tcp/ip related helper functions

---
+++ src/tcp.erl
@@ -0,0 +1,16 @@
+%% module for some tcp/ip related helper functions
+-module(tcp).
+-export([receive_binary/1]).
+-author({"Christopher Bertels", "bakkdoor@flasht.de"}).
+
+
+receive_binary(Socket) ->
+    receive_binary(Socket, []).
+
+receive_binary(Socket, BinAcc) ->
+    case gen_tcp:recv(Socket, 0) of
+	{ok, B} ->
+	    receive_binary(Socket, [BinAcc, B]);
+	{error, closed} ->
+	    {ok, list_to_binary(BinAcc)}
+    end.
